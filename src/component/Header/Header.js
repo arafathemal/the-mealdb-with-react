924849974d3bf2754ca91fedd35c7b8ca7130bdf
arafathemal <arafatfaiyaz@gmail.com>
2022-04-22 17:58:45 +0600

    meal-db

---
+++ src/component/Header/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div className='container-db'>
+            <h1>This Is MeaLDB</h1>
+            <p>I travel to unique parts of the world on the hunt for the best food each country has to offer and share these stories with you in my videos.Hey, I'm Sonny!</p>
+            <small>
+                <b>Best Ever Food Review Show is a YouTube food and travel channel created by American filmmaker Will Sonbuchner, alias, Sonny Side. The production team is based in Vietnam with episodes being filmed globally.</b>
+            </small>
+        </div>
+    );
+};
+
+export default Header;
